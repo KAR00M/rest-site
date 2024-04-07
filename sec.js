8f48f322c84a5a457dabcbe5aa654f4340962157
KAR00M <161459621+KAR00M@users.noreply.github.com>
2024-04-07 16:55:34 +0200

    Add files via upload

---
+++ sec.js
@@ -0,0 +1,36 @@
+let brek = document.getElementById("break");
+let launch = document.getElementById("launch");
+let dinner = document.getElementById("dinner");
+
+let br = document.getElementById("bre");
+let lau = document.getElementById("lau");
+let din = document.getElementById("din");
+
+brek.onclick = function () {
+  lau.style.display = "none";
+  din.style.display = "none";
+  br.style.display = "grid";
+  
+};
+
+launch.onclick = function () {
+  br.style.display = "none";
+  din.style.display = "none";
+  lau.style.display = "grid";
+  brek.style.border = 'none' ;
+};
+dinner.onclick = function () {
+  lau.style.display = "none";
+  br.style.display = "none";
+  din.style.display = "grid";
+  brek.style.border = 'none' ;
+};
+
+
+
+let up=document.getElementById('up');
+
+
+up.onclick=function(){
+  this.scrollTo.up
+}
